16b60a0fa7f5527a7430a87cfb504c09bb211382
Putu Kharisma Widya Iswari <48644776+pkharisma@users.noreply.github.com>
2019-03-30 16:22:49 +0800

    Add files via upload

---
+++ App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+//import Cuaca1 from './src/Cuaca1/Cuaca1';
+import Weather2 from './src/Weather2/Weather2';
+export default class App extends React.Component {
+  render(){
+    return(
+      <Weather2/>
+    );
+  }
+}
